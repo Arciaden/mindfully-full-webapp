9e4b89be6e350b86817d2f30f003efeae5b876c3
Tregan Deveraux <tregandeveraux@Tregans-MacBook-Pro.local>
2022-10-13 17:16:30 -0600

    added a theme folder with some colors and font sizes as well as borders. bult the UI for the sign in page

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,5 +1,46 @@
+import Image from 'next/image'
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from '@chakra-ui/react'
+//need to upload this to a cloud eventually
+import backgroundImage from '../public/yoga-mat.jpeg'
+import SigninForm from '../components/signinForm'
+
+//Im using classnames to make it easier to read. We can access these through css if needed but I'm going to try and avoid that
 const Signin = () => {
-  return <div>Sign in Page</div>
+  return (
+    <Flex
+      className="signin-wrapper"
+      width="100vw"
+      height="100vh"
+      position="relative"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        className="overlay"
+        width="100%"
+        height="100%"
+        position="absolute"
+        backgroundColor="overlay.lightBlue"
+        top="0"
+        left="0"
+        zIndex="1"
+      ></Box>
+      <SigninForm mode="signin" />
+      <Image
+        className="signin-background-image"
+        src={backgroundImage}
+        layout="fill"
+        objectFit="cover"
+      />
+    </Flex>
+  )
 }
 
 export default Signin
---
+++ theme/foundations/borders.js
@@ -0,0 +1,8 @@
+//Border style overrides
+
+export const borders = {
+  borders: {
+    softGray: '1px solid #e2e8f0',
+    textInputActive: '1px solid #2860c6',
+  },
+}
