e6633ffc644528391ccf28cf4324c17ccd6eac54
Tregan Deveraux <tregandeveraux@Tregans-MacBook-Pro.local>
2022-10-23 00:53:39 -0600

    added and if statment to detect the method of the request (POST is the only one so far) so that the API endpoint does not fire at every request

---
+++ pages/api/appointment.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../lib/prisma'
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const {
+      trainerID,
+      clientID,
+      appPlanTitle,
+      appDuration,
+      appNotes,
+      appPlanDesc,
+      type,
+    } = req.body
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: trainerID,
+      },
+    })
+
+    const client = await prisma.client.findUnique({
+      where: {
+        id: clientID,
+      },
+    })
+
+    const appointment = await prisma.appointment.create({
+      data: {
+        userIDs: trainerID,
+        appointmentPlanTitle: appPlanTitle,
+        appointmentDuration: appDuration,
+        appointmentNotes: appNotes,
+        appointmentPlanDescription: appPlanDesc,
+        type,
+      },
+    })
+
+    await prisma.user.update({
+      where: {
+        id: trainerID,
+      },
+      data: {
+        appointments: {
+          connect: {
+            id: appointment.id,
+          },
+        },
+      },
+    })
+
+    res.status(200).json({ user, client, appointment })
+  }
+}
