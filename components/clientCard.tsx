fea71f9e520480e1d91f71026a87853535d4be9a
Tregan Deveraux <tregandeveraux@Tregans-MacBook-Pro.local>
2022-10-29 15:38:48 -0600

    added card flip effect and input fields to the edit side of the client card

--- components/clientCard.tsx
+++ components/clientCard.tsx
@@ -1,20 +1,23 @@
 import {
   Flex,
-  Stack,
   Avatar,
-  Heading,
   Text,
   Box,
   Button,
+  Input,
+  Textarea,
 } from '@chakra-ui/react'
-import { motion, LayoutGroup, AnimatePresence } from 'framer-motion'
+import { EditIcon } from '@chakra-ui/icons'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 import styles from '../styles/clientCard.module.css'
+
+// Framer motion variants objects
 
 const isExpanded = {
   expanded: { height: 240, transition: { duration: 0.5 } },
   closed: {
-    height: 100,
+    height: 143,
     overflow: 'hidden',
     transition: { duration: 0.4 },
   },
@@ -28,103 +31,228 @@ const textEffect = {
   },
 }
 
+const editCard = {
+  edit: {
+    transform: 'rotateY(180deg)',
+    perspective: '500px',
+    transition: { duration: 0.2 },
+  },
+  read: {},
+}
+
 const ClientCard = ({ firstName, lastName, age, bio, email, phone, id }) => {
   const [expanded, setExpanded] = useState(false)
+  const [edit, setEdit] = useState(false)
+
+  //Functions
   const toggleExpanded = () => {
     setExpanded((state) => !state)
+  }
+
+  const toggleEdit = () => {
+    setEdit((state) => !state)
   }
 
   console.log(expanded)
 
   return (
-
-    // <Box
-    //   backgroundColor="yellow"
-    //   width="100%"
-    //   p="21px"
-    //   fontSize="text.xs"
-    //   color="#086F83"
-    //   borderRadius="4px"
-    //   background="linear-gradient(0deg, #2F80ED -45%, rgba(235, 248, 255, 0.47) 63.75%, #FAFDFF 99.24%)"
-    //   box-shadow="3px 4px 7px rgba(0, 0, 0, 0.25)"
-    //   overflow="hidden"
-    // >
-    <motion.div className={styles.clientCardContainer}>
-      <Flex lineHeight={6} mb="10px">
-        <Box mr="20px">
-          <Avatar
-            name={firstName + ' ' + lastName}
-            size="lg"
-            src="https://res.cloudinary.com/deveraux-design/image/upload/v1667011530/Mindfully%20Full/seinfeld_teftvl.jpg"
-          />
-        </Box>
-        <Box>
-          <Flex>
-            <Text fontWeight={700}>
-              {firstName + ' ' + lastName} | {age} | Client
-            </Text>
-          </Flex>
-          <Box fontWeight={500}>
-            <Text>{email}</Text>
-          </Box>
-          <Box>{phone}</Box>
-        </Box>
-      </Flex>
-
-      <Flex fontWeight={400} color="#065666">
-        <Box mr="15px">
-          <Text>BIO:</Text>
-        </Box>
-        <Flex direction="column">
-          {bio.length > 250 && expanded === false ? (
-            <motion.div
-              className="motion-div"
-              initial={'closed'}
-              animate={expanded ? 'expanded' : 'closed'}
-              variants={isExpanded}
-              layout
-            >
-              {bio.substring(0, 200)}...
-            </motion.div>
-          ) : (
-            <motion.div
-              className="motion-div"
-              initial={'closed'}
-              animate={expanded ? 'expanded' : 'closed'}
-              variants={isExpanded}
-              layout
-            >
-              <motion.p>
-                {bio.substring(0, 200)}
-                <motion.span
-                  initial={'closedText'}
-                  animate={expanded ? 'expandedText' : 'closedText'}
-                  variants={textEffect}
-                >
-                  {bio.substring(200, 500)}
-                </motion.span>
-              </motion.p>
-            </motion.div>
-          )}
-          {bio.length > 250 ? (
-            <Box
-              as="button"
-              onClick={toggleExpanded}
-              backgroundColor="transparent"
-              textAlign="left"
-              fontWeight={700}
-              height="20px"
-              mt="5px"
-            >
-              {expanded ? 'Read Less' : 'Continue Reading'}
+    <motion.div
+      className={styles.clientCardContainer}
+      initial={'read'}
+      animate={edit ? 'edit' : 'read'}
+      variants={editCard}
+    >
+      {/* EDIT SIDE OF THE CARD... Maybe try css?  */}
+      {edit ? (
+        <>
+          <Flex lineHeight={6} mb="10px" transform="rotateY(180deg)">
+            <Box mr="20px" position="relative">
+              <Avatar
+                name={firstName + ' ' + lastName}
+                size="lg"
+                src="https://res.cloudinary.com/deveraux-design/image/upload/v1667011530/Mindfully%20Full/seinfeld_teftvl.jpg"
+              />
+              <EditIcon
+                position="absolute"
+                top="0"
+                left="0"
+                height={5}
+                width={5}
+                color="#fff"
+              />
             </Box>
-          ) : (
-            <Box height="20px"></Box>
-          )}
-        </Flex>
-      </Flex>
+            <Box>
+              <Flex>
+                <Input
+                  type="text"
+                  value={firstName}
+                  fontSize={12}
+                  height={8}
+                  m="0 10px 10px 0px"
+                />
+                <Input type="text" value={lastName} fontSize={12} height={8} />
+              </Flex>
+              <Box fontWeight={500} mb="10px">
+                <Input type="email" value={email} fontSize={12} height={8} />
+              </Box>
+              <Input type="tel" value={phone} fontSize={12} height={8} />
+            </Box>
+          </Flex>
+
+          <Flex fontWeight={400} color="#065666" transform="rotateY(180deg)">
+            <Box mr="15px">
+              <Text>BIO:</Text>
+            </Box>
+            <Flex direction="column" width="100%">
+              <Textarea resize="none" fontSize={12} value={bio} mb="20px" />
+              {/* <Flex direction="column">
+              {bio.length > 250 && expanded === false ? (
+                <motion.div
+                  className="motion-div"
+                  initial={'closed'}
+                  animate={expanded ? 'expanded' : 'closed'}
+                  variants={isExpanded}
+                  layout
+                >
+                  {bio.substring(0, 200)}...
+                </motion.div>
+              ) : (
+                <motion.div
+                  className="motion-div"
+                  initial={'closed'}
+                  animate={expanded ? 'expanded' : 'closed'}
+                  variants={isExpanded}
+                  layout
+                >
+                  <motion.p>
+                    {bio.substring(0, 200)}
+                    <motion.span
+                      initial={'closedText'}
+                      animate={expanded ? 'expandedText' : 'closedText'}
+                      variants={textEffect}
+                    >
+                      {bio.substring(200, 500)}
+                    </motion.span>
+                  </motion.p>
+                </motion.div>
+              )}
+              {bio.length > 250 ? (
+                <Box
+                  as="button"
+                  onClick={toggleExpanded}
+                  backgroundColor="transparent"
+                  textAlign="left"
+                  fontWeight={700}
+                  height="20px"
+                  mt="5px"
+                >
+                  {expanded ? 'Read Less' : 'Continue Reading'}
+                </Box>
+              ) : (
+                <Box height="20px"></Box>
+              )}
+              
+            </Flex> */}
+              <Flex width="100%" justifyContent="space-evenly">
+                <Button height={5} fontSize="text.xs" onClick={toggleEdit}>
+                  Cancel
+                </Button>
+                <Button height={5} fontSize="text.xs" onClick={toggleEdit}>
+                  Save
+                </Button>
+              </Flex>
+            </Flex>
+          </Flex>
+        </>
+      ) : (
+        // READ SIDE OF THE CARD
+        <>
+          <Flex lineHeight={6} mb="10px" position="relative">
+            <Box mr="20px">
+              <Avatar
+                name={firstName + ' ' + lastName}
+                size="lg"
+                src="https://res.cloudinary.com/deveraux-design/image/upload/v1667011530/Mindfully%20Full/seinfeld_teftvl.jpg"
+              />
+            </Box>
+            <Box>
+              <Flex>
+                <Text fontWeight={700}>
+                  {firstName + ' ' + lastName} | {age} | Client
+                </Text>
+              </Flex>
+              <Box fontWeight={500}>
+                <Text>{email}</Text>
+              </Box>
+              <Box>{phone}</Box>
+            </Box>
+            <EditIcon
+              aria-label="edit-button"
+              position="absolute"
+              top="0"
+              right="0"
+              _hover={{ cursor: 'pointer' }}
+              onClick={() => setEdit((state) => !state)}
+            >
+              Cancel
+            </EditIcon>
+          </Flex>
+
+          <Flex fontWeight={400} color="#065666">
+            <Box mr="15px">
+              <Text>BIO:</Text>
+            </Box>
+            <Flex direction="column">
+              {bio.length > 250 && expanded === false ? (
+                <motion.div
+                  className="motion-div"
+                  initial={'closed'}
+                  animate={expanded ? 'expanded' : 'closed'}
+                  variants={isExpanded}
+                  layout
+                >
+                  {bio.substring(0, 200)}...
+                </motion.div>
+              ) : (
+                <motion.div
+                  className="motion-div"
+                  initial={'closed'}
+                  animate={expanded ? 'expanded' : 'closed'}
+                  variants={isExpanded}
+                  layout
+                >
+                  <motion.p>
+                    {bio.substring(0, 200)}
+                    <motion.span
+                      initial={'closedText'}
+                      animate={expanded ? 'expandedText' : 'closedText'}
+                      variants={textEffect}
+                    >
+                      {bio.substring(200, 500)}
+                    </motion.span>
+                  </motion.p>
+                </motion.div>
+              )}
+              {bio.length > 250 ? (
+                <Box
+                  as="button"
+                  onClick={toggleExpanded}
+                  backgroundColor="transparent"
+                  textAlign="left"
+                  fontWeight={700}
+                  height="20px"
+                  mt="5px"
+                >
+                  {expanded ? 'Read Less' : 'Continue Reading'}
+                </Box>
+              ) : (
+                <Box height="20px"></Box>
+              )}
+            </Flex>
+          </Flex>
+        </>
+      )}
     </motion.div>
-    // </Box>
   )
 }
 
