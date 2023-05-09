741914ca6be91bad1929b89a7067dcd3bc1aae63
Tregan Deveraux <tregandeveraux@Tregans-MBP.lan>
2023-05-09 13:02:41 -0600

    fixed the sorting on the index page to display appoinments and clients in the correct order

--- components/newClientTable.tsx
+++ components/newClientTable.tsx
@@ -30,21 +30,25 @@ const NewClientTable = () => {
         </Thead>
         <Tbody>
           {user &&
-            user?.clients?.slice(0, 5).map((client) => (
-              <Tr key={client.id}>
-                <Td>
-                  {client.firstName.slice(0, 1).toUpperCase() +
-                    client.firstName.slice(1)}{' '}
-                  {client.lastName.slice(0, 1).toUpperCase() +
-                    client.lastName.slice(1)}
-                </Td>
-                <Td>{client.age}</Td>
-                <Td>{DateTime.fromISO(client.createdAt).toLocaleString()}</Td>
-                <Td>
-                  <Link href={`/userClients/${client.id}`}>View Client</Link>
-                </Td>
-              </Tr>
-            ))}
+            user?.clients
+              ?.slice(0, 5)
+              .sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1))
+              .reverse()
+              .map((client) => (
+                <Tr key={client.id}>
+                  <Td>
+                    {client.firstName.slice(0, 1).toUpperCase() +
+                      client.firstName.slice(1)}{' '}
+                    {client.lastName.slice(0, 1).toUpperCase() +
+                      client.lastName.slice(1)}
+                  </Td>
+                  <Td>{client.age}</Td>
+                  <Td>{DateTime.fromISO(client.createdAt).toLocaleString()}</Td>
+                  <Td>
+                    <Link href={`/userClients/${client.id}`}>View Client</Link>
+                  </Td>
+                </Tr>
+              ))}
         </Tbody>
       </Table>
     </TableContainer>
