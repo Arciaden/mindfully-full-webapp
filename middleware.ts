5dfee592833bb434d8ef578d9cfccad7be247bdb
Tregan Deveraux <tregandeveraux@Tregans-MacBook-Pro.local>
2023-05-10 10:18:16 -0600

    added a check to see if the NODE_ENV was in production or development and redirecting the dev / user to the proper url

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@
 //This is an edge function
 import { NextResponse } from 'next/server'
 
+const prod = process.env.NODE_ENV
+
 //Pages that the middleware will protect
 //This syntax is new to nextjs 12.2.0+
 export const config = {
@@ -11,7 +13,12 @@ export const config = {
 export default function middleware(req) {
   const token = req.cookies.get('MINDFULLY_FULL_ACCESS_TOKEN')
 
-  if (!token) {
+  if (!token && prod === 'production') {
+    return NextResponse.redirect(
+      'https://mindfully-full-webapp-5g2d.vercel.app/signin'
+    )
+  }
+  if (!token && prod === 'development') {
     return NextResponse.redirect('http://localhost:3000/signin')
   }
 }
