85c2d655794bc9a8f40283ed232974fba07589f9
Tregan Deveraux <tregandeveraux@Tregans-MacBook-Pro.local>
2023-05-10 10:23:53 -0600

    middleware adjustment

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@
 //This is an edge function
 import { NextResponse } from 'next/server'
 
-const prod = process.env.NODE_ENV
+const prod = 'production'
 
 //Pages that the middleware will protect
 //This syntax is new to nextjs 12.2.0+
@@ -17,8 +17,5 @@ export default function middleware(req) {
     return NextResponse.redirect(
       'https://mindfully-full-webapp-5g2d.vercel.app/signin'
     )
-  }
-  if (!token && prod === 'development') {
-    return NextResponse.redirect('http://localhost:3000/signin')
   }
 }
